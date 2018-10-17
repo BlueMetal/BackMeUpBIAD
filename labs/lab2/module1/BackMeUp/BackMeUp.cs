d8b493a71df58b4793eb8f5fd30c5ad053d7cf79
Michael Meadows <michaelmeadows@gmail.com>
2018-10-17 07:56:04 -0400

    Lab 2 Module 2 complete

---
+++ labs/lab2/module1/BackMeUp/BackMeUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+
+namespace BackMeUp
+{
+    public class BackMeUp : IBot
+    {
+        private readonly ILogger _logger;
+
+        public BackMeUp(ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(loggerFactory));
+            }
+
+            _logger = loggerFactory.CreateLogger<BackMeUp>();
+            _logger.LogTrace("EchoBot turn start.");
+        }
+
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                var responseMessage = "Hello, World!";
+                await turnContext.SendActivityAsync(responseMessage);
+            }
+            else
+            {
+                await turnContext.SendActivityAsync($"{turnContext.Activity.Type} event detected");
+            }
+        }
+    }
+}
---
+++ labs/lab2/module2/BackMeUp/Dialogs/DialogAccessors.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace BackMeUp.Dialogs
+{
+    public class DialogAccessors
+    {
+        public DialogAccessors(ConversationState conversationState)
+        {
+            ConversationState = conversationState;
+        }
+
+        public static string DialogStateName { get; } = $"{nameof(DialogAccessors)}.{nameof(DialogState)}";
+
+        public IStatePropertyAccessor<DialogState> DialogState { get; set; }
+
+        public ConversationState ConversationState { get; }
+    }
+}
