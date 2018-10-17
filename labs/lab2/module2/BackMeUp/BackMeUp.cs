4b2a44d35623c0980435c3c63cb951bff679e49e
Michael Meadows <michaelmeadows@gmail.com>
2018-10-17 10:56:53 -0400

    first exercise for lab 2 module 2

---
+++ labs/lab2/module2/BackMeUp/BackMeUp.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BackMeUp.Dialogs;
+using BackMeUp.Dialogs.BackPain;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+
+namespace BackMeUp
+{
+    public class BackMeUp : IBot
+    {
+        private readonly ILogger _logger;
+
+        // Module 2
+        private readonly DialogAccessors _accessors;
+        private readonly DialogSet _dialogs;
+
+        // Module 2 add accessors to signature
+        public BackMeUp(DialogAccessors accessors, BackPainDialogFactory backPainDialogFactory, ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(loggerFactory));
+            }
+
+            // Module 2
+            _accessors = accessors;
+            _dialogs = new DialogSet(_accessors.DialogState);
+            _dialogs.Add(backPainDialogFactory.Configure(_dialogs));
+
+            _logger = loggerFactory.CreateLogger<BackMeUp>();
+            _logger.LogTrace("EchoBot turn start.");
+        }
+
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                var dialogContext = await _dialogs.CreateContextAsync(turnContext, cancellationToken);
+                var results = await dialogContext.ContinueDialogAsync(cancellationToken);
+
+                // We are not in a dialog, so resume turns as normal
+                if (results.Status == DialogTurnStatus.Empty)
+                {
+                    var activityText = turnContext.Activity.Text.Trim().ToLowerInvariant();
+
+                    // start the dialog. We'll do better when we integrate LUIS
+                    if (new[] { "back pain", "start" }.Any(t => t == activityText))
+                    {
+                        await dialogContext.BeginDialogAsync(
+                            BackPainDialogFactory.DialogId,
+                            cancellationToken: cancellationToken);
+                    }
+                    else
+                    {
+                        var responseMessage = MessageFactory.Text($"You said \"{turnContext.Activity.Text}\"");
+                        await turnContext.SendActivityAsync(responseMessage, cancellationToken);
+                    }
+                }
+
+                await _accessors.ConversationState.SaveChangesAsync(turnContext, cancellationToken: cancellationToken);
+            }
+            else
+            {
+                await turnContext.SendActivityAsync($"{turnContext.Activity.Type} event detected", cancellationToken: cancellationToken);
+            }
+        }
+    }
+}
---
+++ labs/lab2/module2/BackMeUp/Dialogs/BackPain/BackPainDialogFactory.cs
@@ -0,0 +1,128 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace BackMeUp.Dialogs.BackPain
+{
+    public class BackPainDialogFactory
+    {
+        public const string DialogId = "back-pain";
+
+        private readonly DialogAccessors _accessors;
+
+        public BackPainDialogFactory(DialogAccessors accessors)
+        {
+            _accessors = accessors;
+        }
+
+        public Dialog Configure(DialogSet dialogSet)
+        {
+            RegisterPrompts(dialogSet);
+            var steps = new WaterfallStep[]
+            {
+                ConfirmStartAsync,
+                PromptForAgeAsync,
+                NextStepAsync,
+                FinalStepAsync,
+            };
+            var waterfallDialog = new WaterfallDialog(DialogId, steps);
+            return waterfallDialog;
+        }
+
+        public async Task<DialogTurnResult> ConfirmStartAsync(
+            WaterfallStepContext stepContext,
+            CancellationToken cancellationToken)
+        {
+            // Provides a disclaimer about privacy and prompts the user to agree before continuing
+            await stepContext.Context.SendActivityAsync(
+                MessageFactory.Text(
+                    @"I'm about to ask you some questions to try to determine a course of care for your back pain.
+Some of the questions will be very personal. While we do use the information you provide to improve our ability to help future patients, we will never try to identify individuals or share individual data with anyone."),
+                cancellationToken);
+            await Task.Delay(500, cancellationToken); // half-second between messages feels a little more natural
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text("We value your privacy above all other concerns."), cancellationToken);
+
+            return await stepContext.PromptAsync(Prompts.ConfirmStart, new PromptOptions { Prompt = MessageFactory.Text("Do you wish to proceed?") }, cancellationToken);
+        }
+
+        public async Task<DialogTurnResult> PromptForAgeAsync(
+            WaterfallStepContext stepContext,
+            CancellationToken cancellationToken)
+        {
+            // If the user answered "yes", then we'll continue
+            var proceed = (bool)stepContext.Result;
+            if (proceed)
+            {
+                await stepContext.Context.SendActivityAsync(
+                    MessageFactory.Text("Okay. Let's get started."),
+                    cancellationToken);
+                await Task.Delay(500, cancellationToken);
+            }
+            else
+            {
+                // user answered "no".
+                await stepContext.Context.SendActivityAsync(
+                    MessageFactory.Text("I understand. I hope you feel better."),
+                    cancellationToken);
+                return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+            }
+
+            return await stepContext.PromptAsync(
+                Prompts.Age,
+                new PromptOptions
+                {
+                    Prompt = MessageFactory.Text("How old are you?"),
+                },
+                cancellationToken);
+        }
+
+        public async Task<DialogTurnResult> NextStepAsync(
+            WaterfallStepContext stepContext,
+            CancellationToken cancellationToken)
+        {
+            // get the age from the last prompt
+            var result = (int)stepContext.Result;
+
+            // get the state (or create it if it is new)
+            var state = await _accessors.BackPainDemographics.GetAsync(
+                stepContext.Context,
+                () => new BackPainDemographics(),
+                cancellationToken);
+
+            // set the age in the state object
+            state.Age = result.ToString(CultureInfo.InvariantCulture);
+
+            // for now, we're going to end this step without a prompt so we can see the state persistence
+            // at work in the next step
+            return await stepContext.NextAsync(cancellationToken: cancellationToken);
+        }
+
+        public async Task<DialogTurnResult> FinalStepAsync(
+            WaterfallStepContext stepContext,
+            CancellationToken cancellationToken)
+        {
+            var state = await _accessors.BackPainDemographics.GetAsync(
+                stepContext.Context,
+                () => new BackPainDemographics(),
+                cancellationToken);
+
+            // reflect the user's age back. This is retrieved from state
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Your age is {state.Age}"), cancellationToken);
+            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+        }
+
+        private void RegisterPrompts(DialogSet dialogSet)
+        {
+            dialogSet.Add(new ConfirmPrompt(Prompts.ConfirmStart));
+            dialogSet.Add(new NumberPrompt<int>(Prompts.Age));
+        }
+
+        private static class Prompts
+        {
+            public const string ConfirmStart = DialogId + "_confrim-start";
+            public const string Age = DialogId + "_age";
+        }
+    }
+}
